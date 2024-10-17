d379ddbe1316b5043dfe9a7965234ce5dcc5bd92
joshualee <joshua.leeCS03@gmail.com>
2024-10-17 23:48:01 +0800

    Initial Implem

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import navLogo from "./assets/nav-logo.svg";
+
+import githubIcon from "./assets/github-icon.svg";
+
+import xTwitterIcon from "./assets/x-icon.svg";
+import discordIcon from "./assets/discord-icon.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App d-flex flex-column">
+      <div className="App-header">
+        <button className="btn btn-primary d-flex align-items-center nav-button w-100 bg-transparent">
+          <img src={navLogo} alt="Nav Logo" className="img-fluid" />
+        </button>
+      </div>
+
+      <div className="body-wrapper d-flex flex-fill">
+        <div className="d-flex flex-column text-center">
+          <div className="flex-fill align-content-center mb-4">
+            <p className="landing-text mt-auto">
+              Trade assets and earn by providing liquidity on the most secure
+              decentralized financial system.
+            </p>
+            <button className=" btn btn-primary d-flex align-items-center launch-button mt-4">
+              Launch App
+            </button>
+            <div className="d-flex mt-3">
+              <a className="links mx-2">Learn</a>
+              <a className="links mx-2">Github</a>
+              <a className="links mx-2">Audit</a>
+            </div>
+          </div>
+          <footer className="text-white d-flex mt-auto mb-4">
+            <a className="links mx-2">
+              <img src={githubIcon} alt="GitHub" className="img-fluid" />
+            </a>
+            <a className="links mx-2">
+              <img src={xTwitterIcon} alt="Twitter" className="img-fluid" />
+            </a>
+            <a className="links mx-2">
+              <img src={discordIcon} alt="Discord" className="img-fluid" />
+            </a>
+          </footer>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <div className=" w-100 flex-fill"></div>
   </React.StrictMode>
 );
 
