122d9f46002dc759418628bf1cd2a61ef2f360d7
intrinsiceng <intrinsicnetwork@protonmail.com>
2025-03-31 12:37:15 +0800

    update links

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ import xTwitterIcon from "./assets/x-icon.svg";
 import discordIcon from "./assets/discord-icon.svg";
 
 function App() {
+  const handleLaunchNow = () => {
+    window.open(
+      "https://app.intrinsic.finance/#/swap",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  };
   return (
     <div className="App d-flex flex-column">
       <div className="App-header">
@@ -22,24 +29,55 @@ function App() {
               Trade assets and earn by providing liquidity on the most secure
               decentralized financial system.
             </p>
-            <button className=" btn btn-primary d-flex align-items-center launch-button mt-4">
+            <button
+              className=" btn btn-primary d-flex align-items-center launch-button mt-4"
+              onClick={handleLaunchNow}
+            >
               Launch App
             </button>
             <div className="d-flex mt-3">
-              <a className="links mx-2">Learn</a>
-              <a className="links mx-2">Github</a>
-              <a className="links mx-2">Audit</a>
+              <a
+                className="links mx-2"
+                href="docs.intrinsic.finance" // Replace with your actual GitHub URL
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn
+              </a>
+              <a
+                className="links mx-2"
+                href="https://github.com/Intrinsic-network" // Replace with your actual GitHub URL
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github
+              </a>
+              <a
+                className="links mx-2"
+                href="docs.intrinsic.finance/other-documentation/security-audits/" // Replace with your actual GitHub URL
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Audit
+              </a>
             </div>
           </div>
           <footer className="text-white d-flex mt-auto mb-4">
-            <a className="links mx-2">
+            <a
+              className="links mx-2"
+              href="https://github.com/Intrinsic-network" // Replace with your actual GitHub URL
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img src={githubIcon} alt="GitHub" className="img-fluid" />
             </a>
-            <a className="links mx-2">
+            <a
+              className="links mx-2"
+              href="https://x.com/Intrinsicfi" // Replace with your actual GitHub URL
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img src={xTwitterIcon} alt="Twitter" className="img-fluid" />
-            </a>
-            <a className="links mx-2">
-              <img src={discordIcon} alt="Discord" className="img-fluid" />
             </a>
           </footer>
         </div>
